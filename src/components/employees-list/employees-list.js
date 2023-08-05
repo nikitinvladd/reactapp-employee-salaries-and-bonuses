4051a9805942443f0217095dce437d79c2a07360
Vlad <nikwlad11@gmail.com>
2023-08-05 21:47:52 +0200

    not full ready code

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,25 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeesList = ({data, onDelete}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeesListItem
+                 key={id}
+                 {...itemProps}
+                 onDelete={() => onDelete(id)}
+                 /> //# or this prop name={item.name} salary={item.salary}
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
